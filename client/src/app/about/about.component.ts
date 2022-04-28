fe0f3a42466e051ed63e6137a533db7cd4e5f426
Darina Beneva <beneva.darina@gmail.com>
2022-04-28 18:55:04 +0300

    Implemented back button on About page

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private location: Location) { }
+  
+  back() {
+    this.location.back();
   }
-
 }
